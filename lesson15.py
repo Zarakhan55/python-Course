7757d95935f3d2464a4088b08cc72efe24862e3c
Zara Khan <zarakhang067@gmail.com>
2026-07-30 14:53:48 +0500

    Add files via upload

---
+++ lesson15.py
@@ -0,0 +1,11 @@
+class Animals:
+    def sound(self):
+        print("some animals sound")
+class Child(Animals):
+    def sound(self):
+        print("Dogs are barking!..................")
+dog=Child()
+dog.sound()
+
+
+# =============
---
+++ lesson16.py
@@ -0,0 +1,15 @@
+class Animals:
+    def sound(self):
+        print("meo meo")
+class Dog:
+    def sound(self):
+        print("wooo woo")
+class cats:
+    def sound(self):
+        print("mweo mweo")
+p1=Animals()
+p2=Dog()
+p3=cats()
+p1.sound()
+p2.sound()
+p3.sound()
