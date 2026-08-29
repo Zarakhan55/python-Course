6e4c69e834403671b4d7d4bc889eef592fb8c1a3
Zara Khan <zarakhang067@gmail.com>
2026-08-29 23:13:03 +0500

    Add files via upload

---
+++ electricityBill.py
@@ -0,0 +1,10 @@
+unit=int(input("Enter unit"));
+if unit <=100:
+    bill=unit*10
+    print("Rs 10 per unit",bill)
+elif unit <=200:
+    bill=unit*20
+    print("Rs 20 per unit",bill)
+else:
+    print("unit",bill)
+    
